6cc6940cde094318386690e64ce6fd7802a828e9
Ganga Devi <gangadev001@e.ntu.edu.sg>
2026-09-13 14:53:11 +0800

    Implement random dataset generator in C++
    This program generates datasets of random integers of specified sizes.

---
+++ data_generator.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <random>
+#include <fstream>
+
+using namespace std;
+
+// Generate an array of n random integers in the range [1, x]
+vector<int> generateRandomArray(int n, int x, mt19937& rng) {
+    uniform_int_distribution<int> dist(1, x);
+
+    vector<int> data(n);
+
+    for (int i = 0; i < n; i++) {
+        data[i] = dist(rng);
+    }
+
+    return data;
+}
+
+int main() {
+    // Largest possible value in each dataset
+    const int x = 10000000;
+
+    // Dataset sizes required for the experiment
+    vector<int> sizes = {
+        1000,
+        2000,
+        5000,
+        10000,
+        20000,
+        50000,
+        100000,
+        200000,
+        500000,
+        1000000,
+        2000000,
+        5000000,
+        10000000
+    };
+
+    // Fixed seed so that the experiments are reproducible
+    mt19937 rng(12345);
+
+    cout << "Generating datasets..." << endl;
+    cout << "Values are in the range [1, " << x << "]" << endl;
+    cout << endl;
+
+    for (int n : sizes) {
+        vector<int> data = generateRandomArray(n, x, rng);
+
+        cout << "Generated dataset: n = " << n << endl;
+    }
+
+    cout << endl;
+    cout << "All datasets generated successfully." << endl;
+
+    return 0;
+}
