52a16378429df8ecb9ba6726b7480c2e1ca48130
cherri <cherriannsoo@gmail.com>
2026-09-14 22:47:18 +0800

    changed 3rd graph such that difference is clearer

--- graphs_generation.py
+++ graphs_generation.py
@@ -38,21 +38,32 @@ def plot_s_vs_comparisons(csv_filename):
     print("Saved: graph_s_vs_comparisons.png")
 
 def plot_showdown(csv_filename):
-
+    
     df = pd.read_csv(csv_filename)
 
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
-
+    
     # chart 1: comparisons
-    ax1.bar(df['Algorithm'], df['Comparisons'], color=['#3498db', '#2ecc71'])
+    bars1 = ax1.bar(df['Algorithm'], df['Comparisons'], color=['#3498db', '#2ecc71'])
     ax1.set_title('Key Comparisons (10 Million Integers)')
     ax1.set_ylabel('Comparisons')
-
+    
+    # add exact numbers on top of the comparison bars (clearer)
+    ax1.bar_label(bars1, labels=[f'{x:,.0f}' for x in df['Comparisons']], padding=3, fontweight='bold')
+    
     # chart 2: CPU time
-    ax2.bar(df['Algorithm'], df['CPU_Time_ms'], color=['#e74c3c', '#f1c40f'])
+    bars2 = ax2.bar(df['Algorithm'], df['CPU_Time_ms'], color=['#e74c3c', '#f1c40f'])
     ax2.set_title('CPU Time (10 Million Integers)')
     ax2.set_ylabel('Time (ms)')
-
+    
+    # zoom in on the y-axis so the time difference is visually obvious
+    min_time = df['CPU_Time_ms'].min()
+    max_time = df['CPU_Time_ms'].max()
+    ax2.set_ylim(min_time - 150, max_time + 150) # add a small buffer
+    
+    # add exact millisecond text
+    ax2.bar_label(bars2, labels=[f'{x} ms' for x in df['CPU_Time_ms']], padding=3, fontweight='bold', fontsize=12)
+    
     plt.tight_layout()
     plt.savefig('graph_10m_showdown.png')
     plt.show()
